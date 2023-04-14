927037d32c0fa3a661c602ba5e04d8f6cffa6edd
jmbp1999 <pamudithaj98@gmail.com>
2023-04-14 18:59:14 +0530

    created product posting and getting all the products endpoints

--- product-service/src/main/java/com/jmbp/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/jmbp/productservice/controller/ProductController.java
@@ -2,16 +2,34 @@ package com.jmbp.productservice.controller;
 
 
 import com.jmbp.productservice.dto.ProductRequest;
+import com.jmbp.productservice.dto.ProductResponse;
+import com.jmbp.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
-
+        System.out.println("productRequest arrived");
+             productService.createProduct(productRequest);
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+
+
 }
--- product-service/src/main/java/com/jmbp/productservice/service/ProductService.java
+++ product-service/src/main/java/com/jmbp/productservice/service/ProductService.java
@@ -1,13 +1,18 @@
 package com.jmbp.productservice.service;
 
 import com.jmbp.productservice.dto.ProductRequest;
+import com.jmbp.productservice.dto.ProductResponse;
 import com.jmbp.productservice.model.Product;
 import com.jmbp.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -21,5 +26,17 @@ public class ProductService {
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice()).build();
 
+        productRepository.save(product);
+        log.info("Product {} is saved",product.getId());
+
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List <Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder().id(product.getId()).name(product.getName()).description(product.getDescription()).build();
     }
 }
